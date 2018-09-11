a5652b5d93f8e93151727458b76b6859a2110984
Abdullah Sanver <a_sanver@hotmail.com>
2018-09-11 16:55:33 +0100

    In GreetingControllerTest SpringBootTest annotation replaced with WebMvcTest to only test the web layer with the HomeController,   GreetingController generation tested.

--- src/test/java/com/sanver/trials/springboot_hello_rest_source/GreetingControllerTest.java
+++ src/test/java/com/sanver/trials/springboot_hello_rest_source/GreetingControllerTest.java
@@ -1,13 +1,14 @@
 package com.sanver.trials.springboot_hello_rest_source;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sanver.trials.springboot_hello_rest_source.controllers.GreetingController;
 import com.sanver.trials.springboot_hello_rest_source.models.GreetingDTO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -16,6 +17,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import static com.sanver.trials.springboot_hello_rest_source.models.Helpers.getIp;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -23,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
-@SpringBootTest
+@WebMvcTest(GreetingController.class)
 @ConfigurationProperties(prefix = "greeting")
 public class GreetingControllerTest {
     @Autowired
@@ -32,10 +34,18 @@ public class GreetingControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private GreetingController controller;
+
     private String greetingMessage;
 
     public void setGreetingMessage(String greetingMessage) {
         this.greetingMessage = greetingMessage;
+    }
+
+    @Test
+    public void should_LoadContext() {
+        assertNotNull(controller);
     }
 
     @Test
