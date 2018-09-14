6ff0b49b57cbf31c24b6a2b7a627891382296cdd
Abdullah Sanver <a_sanver@hotmail.com>
2018-09-14 21:02:43 +0100

    Post method added to GreetingController with its test.

--- src/main/java/com/sanver/trials/springboot_hello_rest_source/controllers/GreetingController.java
+++ src/main/java/com/sanver/trials/springboot_hello_rest_source/controllers/GreetingController.java
@@ -2,9 +2,7 @@ package com.sanver.trials.springboot_hello_rest_source.controllers;
 
 import com.sanver.trials.springboot_hello_rest_source.models.GreetingDTO;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -17,16 +15,30 @@ import static com.sanver.trials.springboot_hello_rest_source.models.Helpers.getI
 @ConfigurationProperties(prefix = "greeting")
 public class GreetingController {
     private String greetingMessage;
+    private String greetingPostMessage;
 
     public void setGreetingMessage(String greetingMessage) {
         this.greetingMessage = greetingMessage;
     }
 
-    @RequestMapping("/greet")
+    public void setGreetingPostMessage(String greetingPostMessage) {
+        this.greetingPostMessage = greetingPostMessage;
+    }
+
+    @GetMapping("/greet")
     public GreetingDTO greet(@RequestParam("name") String name, @RequestParam("birthdate") String birthDateString) {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
         LocalDate birthDate = LocalDate.parse(birthDateString, dateTimeFormatter);
         int age = birthDate.until(LocalDate.now()).getYears();
         return new GreetingDTO(name, String.format(greetingMessage, name, age, birthDate), age, birthDate, LocalTime.now(), getIp());
+    }
+
+    @PostMapping("/greet")
+    public GreetingDTO greetWithPost(@RequestBody GreetingDTO greetingDTO) {
+        LocalDate birthDate = greetingDTO.getBirthDate();
+        int age = birthDate.until(LocalDate.now()).getYears();
+        return new GreetingDTO(greetingDTO.getName(), String.format(greetingPostMessage, greetingDTO.getName(),
+                greetingDTO.getAge(), greetingDTO.getBirthDate()),
+                age, birthDate, LocalTime.now(), getIp());
     }
 }
--- src/test/java/com/sanver/trials/springboot_hello_rest_source/controllers/GreetingControllerTest.java
+++ src/test/java/com/sanver/trials/springboot_hello_rest_source/controllers/GreetingControllerTest.java
@@ -18,6 +18,7 @@ import java.time.LocalTime;
 import static com.sanver.trials.springboot_hello_rest_source.models.Helpers.getIp;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,9 +38,14 @@ public class GreetingControllerTest {
     private GreetingController controller;
 
     private String greetingMessage;
+    private String greetingPostMessage;
 
     public void setGreetingMessage(String greetingMessage) {
         this.greetingMessage = greetingMessage;
+    }
+
+    public void setGreetingPostMessage(String greetingPostMessage) {
+        this.greetingPostMessage = greetingPostMessage;
     }
 
     @Test
@@ -52,14 +58,31 @@ public class GreetingControllerTest {
         String name = "Ahmet";
         LocalDate birthDate = LocalDate.of(2016, 3, 10);
         int age = birthDate.until(LocalDate.now()).getYears();
-        GreetingDTO expectedClass = new GreetingDTO(name, String.format(greetingMessage, name, age, birthDate),
-                age, birthDate, LocalTime.now(), getIp());
         ResultActions perform = mockMvc.perform(get(String.format("/greeting/greet?name=%s&birthdate=%s",
-                name,birthDate.toString().replaceAll("-",""))));
+                name, birthDate.toString().replaceAll("-", ""))));
         perform.andDo(print()).andExpect(status().isOk());
         String contentAsString = perform.andReturn().getResponse().getContentAsString();
         GreetingDTO actual = objectMapper.readValue(contentAsString, GreetingDTO.class);
-        expectedClass.setTime(actual.getTime());
+        GreetingDTO expectedClass = new GreetingDTO(name, String.format(greetingMessage, name, age, birthDate),
+                age, birthDate, actual.getTime(), getIp());
+        String expectedJSON = objectMapper.writeValueAsString(expectedClass);
+        perform.andExpect(content().json(expectedJSON));
+    }
+
+    @Test
+    public void should_ReturnMessageWithAgain_WhenPost() throws Exception {
+        String name = "Ahmet";
+        LocalDate birthDate = LocalDate.of(2016, 3, 10);
+        int age = birthDate.until(LocalDate.now()).getYears();
+        GreetingDTO inputDTO = new GreetingDTO(name, String.format(greetingMessage, name, age, birthDate),
+                age, birthDate, LocalTime.now(), getIp());
+        ResultActions perform = mockMvc.perform(post("/greeting/greet").contentType("application/json").
+                content(objectMapper.writeValueAsBytes(inputDTO)));
+        perform.andDo(print()).andExpect(status().isOk());
+        String contentAsString = perform.andReturn().getResponse().getContentAsString();
+        GreetingDTO actual = objectMapper.readValue(contentAsString, GreetingDTO.class);
+        GreetingDTO expectedClass = new GreetingDTO(name, String.format(greetingPostMessage, name, age, birthDate), age,
+                birthDate, actual.getTime(), getIp());
         String expectedJSON = objectMapper.writeValueAsString(expectedClass);
         perform.andExpect(content().json(expectedJSON));
     }
